3660e21de11db1430176e60177d332a1f54dbcdd
ladayjawynn26 <55443422+ladayjawynn26@users.noreply.github.com>
2019-09-19 08:42:13 -0400

    Add files via upload

---
+++ 2019-09-19/2019-09-19/main.cpp
@@ -0,0 +1,12 @@
+//Ladayja Wynn
+//Intro to Computer Science 
+//Period 1
+//Mr. Williams 
+//Classwork 2019-09-19
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Name:Ladayja Wynn" << std::endl; << "Period 1:Computer Science";
+}
