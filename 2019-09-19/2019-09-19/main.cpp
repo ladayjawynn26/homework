e67f66d35a7ef40b8f3d3a69114c47deb73e2eb3
ladayjawynn26 <55443422+ladayjawynn26@users.noreply.github.com>
2019-09-20 08:33:33 -0400

    Update main.cpp

--- 2019-09-19/2019-09-19/main.cpp
+++ 2019-09-19/2019-09-19/main.cpp
@@ -8,5 +8,16 @@
 
 int main()
 {
-	std::cout << "Name:Ladayja Wynn" << std::endl; << "Period 1:Computer Science";
+	std::cout << "Name:Ladayja Wynn" << std::endl; 
+	<< "Period 1: Computer Science"; << std::endl
+	<< "Period 2: Algebra 2"; <<std::endl
+	<< "Period 3: "Gym"; <<std::endl
+	<< "Period 4: "Health"; <<std::endl
+	<< "Period 5: "Lunch"; <<std::endl
+	<< "Period 6: "English 10 Honors; <<std::endl
+	<< "Period 7: "House"; <<std::endl
+	<< "Period 8: "Global History" <<std::endl
+	<< "Period 9: "Spanish 2" <<std::endl
+	<< "Period 10: "Forensics" <<std::endl
+	
 }
